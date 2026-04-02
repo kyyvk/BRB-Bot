fb5ed372377f83cda1c1422fbe310441b5aafd14
Key Usaju <keyusaju0@gmail.com>
2026-04-02 22:21:15 +0700

    reset slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -23,9 +23,9 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
     console.log('🔄 Registering slash commands...');
 
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), // global command
-      { body: commands }
-    );
+  Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+  { body: [] }
+);
 
     console.log('✅ Slash commands berhasil didaftarkan!');
   } catch (error) {
